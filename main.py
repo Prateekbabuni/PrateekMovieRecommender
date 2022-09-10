8ff1e9a18f343349d26a9ca7d4b4b42cc8609b4e
Prateekbabuni <113231449+Prateekbabuni@users.noreply.github.com>
2022-09-10 19:39:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import pickle
+import streamlit as st
+import pandas as pd
+import requests
+
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('a.jpg')
+
+
+
+
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=76a1ec42763e110dbcd2dd114cfbfd1a&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.loc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+movies_dict=pickle.load(open('movie_dict.pkl','rb'))
+similarity=pickle.load(open('similarity.pkl','rb'))
+movies=pd.DataFrame(movies_dict)
+st.title('Prateek Movie Recommendation System')
+# st.markdown("<h1 style='text-align: center; color: red;'>Prateek Movie Recommendation System</h1>", unsafe_allow_html=True)
+option=st.selectbox('Select your Liked Movie',movies['title'].values)
+
+col1, col2, col3 = st.columns(3)
+if st.button('Click Here'):
+    recommended_movie_names,recommended_movie_posters = recommend(option)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
